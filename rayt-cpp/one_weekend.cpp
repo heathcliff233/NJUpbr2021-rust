67d3fd29b2216190917f7894e17e0586c8a2ffa0
Bright Hong <brighthong233@gmail.com>
2021-03-18 20:58:53 +0800

    benchmark

--- rayt-cpp/one_weekend.cpp
+++ rayt-cpp/one_weekend.cpp
@@ -88,9 +88,9 @@ hittable_list random_scene() {
 
 int main() {
   const auto aspect_ratio = 16.0 / 9.0;
-  const int image_width = 600;
+  const int image_width = 1200;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
-  const int samples_per_pixel = 20;
+  const int samples_per_pixel = 10;
   const int max_depth = 50;
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
